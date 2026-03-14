aa04295d78ed71b39a8433d60fdace74dd8cea6b
anandarbone <anand.arbone@gmail.com>
2026-03-14 20:02:29 +0100

    up

--- id.tf
+++ id.tf
@@ -2,7 +2,7 @@
 # Create user assigned managed identity
 #------------------------------------------------------------------
 
-resource "azurerm_user_assigned_identity" "uai" {
+resource "azurerm_user_assigned_identity" "id" {
   name                = local.user_assigned_identity_name
   location            = var.location
   resource_group_name = var.resource_group_name
